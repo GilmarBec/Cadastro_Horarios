d9c5d5134f06ee0f413c27caa11e958f8e209263
alexrms <ricdoa2@gmail.com>
2018-04-10 00:08:12 -0300

    Fixed bugs into login

--- master/php/loginControl.php
+++ master/php/loginControl.php
@@ -1,7 +1,4 @@
 <?php
-  include_once("../lib/session.php");
-  if($logado) Header("Location: ../");
-  
   include_once("../lib/conexao.php");
   include_once("../dao/usuarioDao.php");
   include_once("../domain/usuario.php");
@@ -13,13 +10,13 @@
   $usuario->setLogin($_POST["login"]);
   $usuario->setSenha(md5($_POST["senha"]));
   
-  if($usuarioDao->login($usuario, $conexao)) {
-    $linha = $usuarioDao->buscar($usuario, $conexao->getCon());
+  if($usuarioDao->login($usuario)) {
+    $linha = $usuarioDao->buscar($usuario);
     
     session_start();
-    $_SESSION['id'] = $linha[id];
-    $_SESSION['nome'] = $linha[nome];
-    $_SESSION['login'] = $linha[login];
+    $_SESSION['id'] = $linha['id'];
+    $_SESSION['nome'] = $linha['nome'];
+    $_SESSION['login'] = $linha['login'];
     
     Header("Location: ../");
   } else {
