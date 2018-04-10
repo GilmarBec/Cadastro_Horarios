e01eea6b31897d76dd95c980098b68c6ab04df1d
GilmarBec <trabalhos4.5.1.4@gmail.com>
2018-04-09 22:36:43 -0300

    Add update and select on 'master/dao/usuarioDao.php'

--- master/dao/usuarioDao.php
+++ master/dao/usuarioDao.php
@@ -13,6 +13,22 @@
       $this->con->prepare($sql)->execute($user);
       echo '<script>alert("Usuario Adicionado com sucesso!");</script>';
     }
+
+    function select(){
+      try{
+        $stmt = $this->con->prepare('SELECT id, login FROM usuario');
+        $stmt->execute();
+
+        $linha = null;
+        foreach($stmt as $row) {
+          $linha = $row;
+        }
+
+        return $linha;
+      } catch(PDOException $e){
+        echor 'ERROR: ' . $e->getMessage();
+      }
+    }
     
     function buscar($usuario){
       try {
@@ -46,13 +62,43 @@
       }
     }
       
-      //return resposta
+    //return resposta
     function login($usuario) {
       $res = $this->selectLogin($usuario);
       if($res != null && $res == $usuario->getSenha()) {
         return true;
       } else {
         return false;
+      }
+    }
+
+    function update($usuario){
+      try {
+        $sql = 'SELECT id FROM usuarios WHERE login=:login';
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute($usuario);
+        
+        $linha = null;
+        foreach($stmt as $row) {
+          $linha = $row;
+        }
+        
+        if($linha != null) {
+          return false;
+        }
+      } catch(PDOException $e) {
+        return $e->getMessage();
+      }
+
+      try {
+        $array = array('id'=>$usuario->getId(), 'login'=>$usuario->getLogin(), 'senha'=>$usuario->getSenha());
+        $sql = 'UPDATE usuario SET login=:login AND senha=:senha WHERE id=:id';
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute($array);
+        
+        return true;
+      } catch(PDOException $e) {
+        return $e->getMessage();
       }
     }
   }
