61b0cf629684d4b88bb6f49c3a5d64b149364c2f
alexrms <ricdoa2@gmail.com>
2018-04-10 14:17:21 -0300

    Change block in the 'horarios' to 'tipos' from users not admin

--- master/lib/navbar.php
+++ master/lib/navbar.php
@@ -17,23 +17,23 @@
           <ul class="nav navbar-nav">
             <li <?php if($pageAtual == "home") echo ' class="active"';?>><a href="/master/">Home</a></li>
             <li><a href="/horarios/">Horários</a></li>
-            <li class="dropdown<?php if($pageAtual == "cadastros") echo ' active'; ?>"<?php if(!$logado) echo ' style="display: none;"';?>>
+            <li class="dropdown<?php if($pageAtual == "cadastros") echo ' active'; ?>">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cadastros <span class="caret"></span></a>
               <ul class="dropdown-menu" id="dropdown">
-                <li<?php if($_SESSION['login'] != "admin") echo ' style=" display: none;"'; ?>><a href="/master/cadastroHorarios.php">Cadastro de Horários</a></li>
-                <li<?php if($_SESSION['login'] != "admin") echo ' style=" display: none;"'; ?> role="separator" class="divider"></li>
+                <li><a href="/master/cadastroHorarios.php">Cadastro de Horários</a></li>
+                <li role="separator" class="divider"></li>
                 <li><a href="/master/cadastroProfessores.php">Cadastro de Professores</a></li>
                 <li role="separator" class="divider"></li>
                 <li><a href="/master/cadastroSalas.php">Cadastro de Salas</a></li>
                 <li role="separator" class="divider"></li>
-                <li><a href="/master/cadastroTipos.php">Cadastro de Tipos</a></li>
-                <li role="separator" class="divider"></li>
+                <li<?php if($_SESSION['login'] != "admin") echo ' style=" display: none;"'; ?>><a href="/master/cadastroTipos.php">Cadastro de Tipos</a></li>
+                <li<?php if($_SESSION['login'] != "admin") echo ' style=" display: none;"'; ?> role="separator" class="divider"></li>
                 <li><a href="/master/cadastroTurmas.php">Cadastro de Turmas</a></li>
               </ul>
             </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li<?php if($pageAtual == "settings") echo ' class="active"'; if(!$logado || $_SESSION['login'] != "admin") echo ' style=" display: none;"';?>><a href="/master/settings/">Settings</a></li>
+            <li<?php if($pageAtual == "settings") echo ' class="active"'; if($_SESSION['login'] != "admin") echo ' style=" display: none;"';?>><a href="/master/settings/">Settings</a></li>
             <li<?php if(!$logado) echo ' style=" display: none;"';?>><a href="/master/sse/sseUpdate.php">Update</a></li>
             <li<?php if(!$logado) echo ' style=" display: none;"';?>><a href="/master/php/logoutControl.php">Logout</a></li>
           </ul>
