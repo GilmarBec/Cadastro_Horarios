17a712db99e0dd5cef9cfe778a0e6bcb927ca950
alexrms <ricdoa2@gmail.com>
2018-04-12 00:24:50 -0300

    Testes

--- master/teste.php
+++ master/teste.php
@@ -12,14 +12,22 @@
 </html>
 
 <?php
+	function csvConvert($array) {
+		return str_getcsv($array, ",");
+	}
+	
+
 	if(isset($_FILES['file'])) {
 		$file = $_FILES['file'];
 		$f = fopen($file['tmp_name'], "r");
-		$csv = array_map('str_getcsv', file($file['tmp_name']));
-		for ($i = 1; $i < count($csv) - 1; $i++) {
-			$col = explode(";", $csv[$i][0]);
+		$csv = array_map('csvConvert', file($file['tmp_name']));
+		$array = [];
+		for ($i = 1; $i < count($csv); $i++) {
+			for ($j = 0; $j < 23; $j++) { 
+				$array[$i-1] = $csv[$i];
+			}
 			$j = 1;
-			foreach ($col as $value) {
+			// foreach ($col as $value) {
 				// switch($j) {
 				// 	case 2:
 				// 	case 3:
@@ -31,18 +39,30 @@
 				// 		break;
 				// 	default: break;
 				// }
-				echo $value;
-			}
-			// echo $csv[$i][0];
+				// echo $value;
+				// $j++;
+			// }
 		}
+		foreach ($array as $row) {
+			// echo $row[2] . " / ";
+			// echo $row[3] . " / ";
+			// echo $row[6] . " / ";
+			// echo $row[9] . " / ";
+			// echo $row[11] . " / ";
+			// echo $row[16] . " / ";
+			// echo $row[17] . " / ";
+			// echo '<br><br><br><br>';
+			echo count($row) . "<br>";
+		}
+		// echo count($csv);
 		fclose($f);
 	}
 ?>
 
-2
+<!-- 2
 3
 6
 9
 11
 16
-17
+17 -->
