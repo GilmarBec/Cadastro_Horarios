326c45771621e1882319861b06e4512b65323e18
AlexRMS <ricdoa2@gmail.com>
2018-04-11 22:28:43 -0300

    Updating teste

--- master/teste.php
+++ master/teste.php
@@ -1,25 +1,48 @@
-<?php
-	if(isset($_POST['file'])) {
-		$_FILES['file'] = $_POST['file'];
-		$file = $_FILES;
-		print_r($file);
-		// fopen($file, "r");
-		// print_r(fgetcsv($file));
-		// fclose($file);
-	} else {
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Teste XLS</title>
 </head>
 <body>
-	<form method="post" encytype = "multipart / form-data">
-		<input id="file" type="file" name="file" multiple="off"><br>
-		<button type="submit">Enviar</button>
+	<form method="POST" enctype="multipart/form-data">
+		<input id="file" type="file" name="file"><br>
+		<input type="submit" value="Enviar">
 	</form>
 </body>
 </html>
+
 <?php
+	if(isset($_FILES['file'])) {
+		$file = $_FILES['file'];
+		$f = fopen($file['tmp_name'], "r");
+		$csv = array_map('str_getcsv', file($file['tmp_name']));
+		for ($i = 1; $i < count($csv) - 1; $i++) {
+			$col = explode(";", $csv[$i][0]);
+			$j = 1;
+			foreach ($col as $value) {
+				// switch($j) {
+				// 	case 2:
+				// 	case 3:
+				// 	case 6:
+				// 	case 9:
+				// 	case 11:
+				// 	case 16:
+				// 	case 17:
+				// 		break;
+				// 	default: break;
+				// }
+				echo $value;
+			}
+			// echo $csv[$i][0];
+		}
+		fclose($f);
 	}
 ?>
+
+2
+3
+6
+9
+11
+16
+17
