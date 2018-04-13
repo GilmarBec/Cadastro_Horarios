e556fde7ee7ba2aa9d7e0d77f0e7054d253877c8
alexrms <ricdoa2@gmail.com>
2018-04-13 14:23:58 -0300

    Implements imports

--- master/importar/controller/professoresControl.php
+++ master/importar/controller/professoresControl.php
@@ -1,49 +1,74 @@
 <?php
-	if(isset($_FILES['file'])) {
-		$file = $_FILES['file'];
-		$f = fopen($file['tmp_name'], "r");
-		$csv = array_map('str_getcsv', file($file['tmp_name']));
-		$array = [];
-		for ($i = 1; $i < count($csv); $i++) {
-			for ($j = 0; $j < 23; $j++) { 
-				$array[$i-1] = $csv[$i];
-			}
-		}
-		$values = [];
-		$cont = 0;
-		foreach ($array as $row) {
-			if(count($row) == 8 && $row[7] != "-") {
-				$values[$cont][0] = $row[3];
-				$values[$cont][1] = $row[6];
-				$cont--;
-			} else if(count($row) == 16 && $row[9] != "-") {
-				$values[$cont][2] = $row[2];
-				$values[$cont][3] = $row[4];
-				$values[$cont][4] = $row[9];
-				$values[$cont][5] = substr($row[10], -3);
-			} else if(count($row) == 23 && $row[3] != "-") {
-				$values[$cont][0] = $row[3];
-				$values[$cont][1] = $row[6];
-				$values[$cont][2] = $row[9];
-				$values[$cont][3] = $row[11];
-				$values[$cont][4] = $row[16];
-				if(substr($row[17], -3) == "CIG") $values[$cont][5] = "-----";
-				else $values[$cont][5] = substr($row[17], -3);
-			} else $cont--;
-			$cont ++;
-		}
-		$professores = [];
-		foreach ($values as $linha) {
-			array_push($professores, $linha[4]);
-		}
-		$professores = array_unique($professores);
-		asort($professores);
-		echo '<br><b>Professores</b><br>';
-		foreach ($professores as $professor) {
-			$professor = explode(" ", $professor)[0] . " " . explode(" ", $professor)[count(explode(" ", $professor))-1];
-			echo $professor . '<br>';
-		}
+	include_once('../../lib/conexao.php');
+	include_once('../../dao/professorDao.php');
+	include_once('../../domain/professor.php');
 
-		fclose($f);
+	$conexao = new Conexao();
+	$professorDao = new ProfessorDao($conexao);
+
+	$file = $_FILES['file'];
+	$f = fopen($file['tmp_name'], "r");
+	$csv = array_map('str_getcsv', file($file['tmp_name']));
+	$array = [];
+	for ($i = 1; $i < count($csv); $i++) {
+		for ($j = 0; $j < 23; $j++) { 
+			$array[$i-1] = $csv[$i];
+		}
 	}
+	$values = [];
+	$cont = 0;
+	foreach ($array as $row) {
+		if(count($row) == 8 && $row[7] != "-") {
+			$values[$cont][0] = $row[3];
+			$values[$cont][1] = $row[6];
+			$cont--;
+		} else if(count($row) == 16 && $row[9] != "-") {
+			$values[$cont][2] = $row[2];
+			$values[$cont][3] = $row[4];
+			$values[$cont][4] = $row[9];
+			$values[$cont][5] = substr($row[10], -3);
+		} else if(count($row) == 23 && $row[3] != "-") {
+			$values[$cont][0] = $row[3];
+			$values[$cont][1] = $row[6];
+			$values[$cont][2] = $row[9];
+			$values[$cont][3] = $row[11];
+			$values[$cont][4] = $row[16];
+			if(substr($row[17], -3) == "CIG") $values[$cont][5] = "-----";
+			else $values[$cont][5] = substr($row[17], -3);
+		} else $cont--;
+		$cont ++;
+	}
+	$professores = [];
+	foreach ($values as $linha) {
+		array_push($professores, $linha[4]);
+	}
+	$professores = array_unique($professores);
+	asort($professores);
+	
+	try {
+    $stmt = $conexao->getCon()->prepare('DROP TABLE professor');
+    $stmt->execute();
+
+    $sql = "
+	    CREATE TABLE IF NOT EXISTS `professor` (
+			  `id` int(11) AUTO_INCREMENT,
+			  `nome` varchar(255) NOT NULL,
+			  PRIMARY KEY (`id`)
+			);
+		";
+    $stmt = $conexao->getCon()->prepare($sql);
+    $stmt->execute();
+  } catch(PDOException $e) {
+    echo 'ERRO: ' . $e->getMessage();
+  }
+
+	foreach ($professores as $result) {
+		$professor = new Professor();
+		$professor->setNome(explode(" ", $result)[0] . " " . explode(" ", $result)[count(explode(" ", $result))-1]);
+		$professorDao->insert($professor);
+	}
+
+	fclose($f);
+
+	Header('location: ../');
 ?>
--- master/importar/controller/salasControl.php
+++ master/importar/controller/salasControl.php
@@ -1,47 +1,74 @@
 <?php
-	if(isset($_FILES['file'])) {
-		$file = $_FILES['file'];
-		$f = fopen($file['tmp_name'], "r");
-		$csv = array_map('str_getcsv', file($file['tmp_name']));
-		$array = [];
-		for ($i = 1; $i < count($csv); $i++) {
-			for ($j = 0; $j < 23; $j++) { 
-				$array[$i-1] = $csv[$i];
-			}
+	include_once('../../lib/conexao.php');
+	include_once('../../dao/salaDao.php');
+	include_once('../../domain/sala.php');
+
+	$conexao = new Conexao();
+	$salaDao = new SalaDao($conexao);
+
+	$file = $_FILES['file'];
+	$f = fopen($file['tmp_name'], "r");
+	$csv = array_map('str_getcsv', file($file['tmp_name']));
+	$array = [];
+	for ($i = 1; $i < count($csv); $i++) {
+		for ($j = 0; $j < 23; $j++) { 
+			$array[$i-1] = $csv[$i];
 		}
-		$values = [];
-		$cont = 0;
-		foreach ($array as $row) {
-			if(count($row) == 8 && $row[7] != "-") {
-				$values[$cont][0] = $row[3];
-				$values[$cont][1] = $row[6];
-				$cont--;
-			} else if(count($row) == 16 && $row[9] != "-") {
-				$values[$cont][2] = $row[2];
-				$values[$cont][3] = $row[4];
-				$values[$cont][4] = $row[9];
-				$values[$cont][5] = substr($row[10], -3);
-			} else if(count($row) == 23 && $row[3] != "-") {
-				$values[$cont][0] = $row[3];
-				$values[$cont][1] = $row[6];
-				$values[$cont][2] = $row[9];
-				$values[$cont][3] = $row[11];
-				$values[$cont][4] = $row[16];
-				if(substr($row[17], -3) == "CIG") $values[$cont][5] = "-----";
-				else $values[$cont][5] = substr($row[17], -3);
-			} else $cont--;
-			$cont ++;
-		}
-		$salas = [];
-		foreach ($values as $linha) {
-			array_push($salas, $linha[5]);
-		}
-		$salas = array_unique($salas);
-		asort($salas);
-		echo '<br><b>Salas</b><br>';
-		foreach ($salas as $sala) {
-			echo $sala . '<br>';
-		}
-		fclose($f);
 	}
+	$values = [];
+	$cont = 0;
+	foreach ($array as $row) {
+		if(count($row) == 8 && $row[7] != "-") {
+			$values[$cont][0] = $row[3];
+			$values[$cont][1] = $row[6];
+			$cont--;
+		} else if(count($row) == 16 && $row[9] != "-") {
+			$values[$cont][2] = $row[2];
+			$values[$cont][3] = $row[4];
+			$values[$cont][4] = $row[9];
+			$values[$cont][5] = substr($row[10], -3);
+		} else if(count($row) == 23 && $row[3] != "-") {
+			$values[$cont][0] = $row[3];
+			$values[$cont][1] = $row[6];
+			$values[$cont][2] = $row[9];
+			$values[$cont][3] = $row[11];
+			$values[$cont][4] = $row[16];
+			if(substr($row[17], -3) == "CIG") $values[$cont][5] = "-----";
+			else $values[$cont][5] = substr($row[17], -3);
+		} else $cont--;
+		$cont ++;
+	}
+	$salas = [];
+	foreach ($values as $linha) {
+		array_push($salas, $linha[5]);
+	}
+	$salas = array_unique($salas);
+	asort($salas);
+
+	try {
+	  $stmt = $conexao->getCon()->prepare('DROP TABLE sala');
+	  $stmt->execute();
+
+	  $sql = "
+	    CREATE TABLE IF NOT EXISTS `sala` (
+			  `id` int(11) AUTO_INCREMENT,
+			  `nome` varchar(255) NOT NULL,
+			  PRIMARY KEY (`id`)
+			);
+		";
+	  $stmt = $conexao->getCon()->prepare($sql);
+	  $stmt->execute();
+	} catch(PDOException $e) {
+	  echo 'ERRO: ' . $e->getMessage();
+	}
+
+	foreach ($salas as $result) {
+		$sala = new Sala();
+		$sala->setNome($result);
+		$salaDao->insert($sala);
+	}
+
+	fclose($f);
+
+	Header('location: ../');
 ?>
